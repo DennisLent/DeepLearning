64c6f723c8392ac8027d8d57fcab1a64ecb531f3
DennisLent <62008839+DennisLent@users.noreply.github.com>
2023-03-14 15:11:48 +0100

    deleted old csv files

---
+++ ImageRecognition.py
@@ -0,0 +1,14 @@
+""" Airplanes ✈️
+Cars 🚗
+Birds 🐦
+Cats 😺
+Deer 🐆
+Dogs 🐶
+Frogs 🐸
+Horses 🐴
+Ships 🚢
+Trucks 🚚 """
+
+import tensorflow as tf
+from tensorflow.keras import datasets, layers, models
+import matplotlib.pyplot as plt
