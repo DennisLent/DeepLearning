710747dee04abafee2c6ddaca3d1293df5eb7493
DennisLent <62008839+DennisLent@users.noreply.github.com>
2023-03-14 12:51:25 +0100

    redid data set

--- FeedForward.py
+++ FeedForward.py
@@ -1,14 +1,24 @@
 from tensorflow import keras
+from keras.models import Sequential
+from keras import Input
+from keras.layers import Dense
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split as tts
 
-#setting up the data
+#Set up the data
 
-df = pd.read_csv("seattle-weather.csv")
-df["weather"].replace(['drizzle', 'rain', 'sun', 'snow', 'fog'], [0,1,2,3,4], inplace=True)
+df = pd.read_csv("cardio.csv", delimiter=";")
+print(df)
 
-x = df[["precipitation", "temp_max", "temp_min", "wind"]]
-y = df["weather"]
+x = df[["age", "gender", "height", "weight", "ap_hi", "ap_lo", "smoke", "alco", "active"]]
+y = df["cardio"]
+
 
 x_train, x_test, y_train, y_test = tts(x, y, test_size=0.33, random_state=23)
+print(x_train, y_train)
+
+#Set up Feed Forward Network 
+
+
+
