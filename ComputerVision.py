80f1231e11f0e04a1b9b5d2bfa8ad3328863235f
DennisLent <62008839+DennisLent@users.noreply.github.com>
2023-03-15 12:47:25 +0100

    accuracy update
    40%

--- ComputerVision.py
+++ ComputerVision.py
@@ -6,7 +6,7 @@ from keras import layers, models
 from keras.utils import to_categorical
 import matplotlib.pyplot as plt
 
-image_size = 64
+image_size = 75
 
 data, categories = create_data(image_size)
 #print(data[0], np.shape(data[0][0]))
@@ -21,32 +21,35 @@ for features, label in data:
     X.append(features)
     y.append(label)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=23)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=23)
 X_train, X_test = np.array(X_train), np.array(X_test)
 y_train, y_test = to_categorical(y_train, num_classes=num_categories), to_categorical(y_test, num_classes=num_categories)
 
 
 #build model
 model = models.Sequential(name="AnimalRecognition")
-model.add(layers.Conv2D(32, (4,4), padding="same", input_shape=(image_size, image_size, 3)))
-model.add(layers.Activation("relu"))
-model.add(layers.Conv2D(128, (4,4)))
-model.add(layers.Activation("relu"))
-model.add(layers.MaxPool2D(pool_size=(2,2)))
-model.add(layers.Dropout(0.25))
+model.add(layers.Conv2D(32, (3, 3), padding="same", activation='relu', input_shape=(image_size, image_size, 3)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
 
 model.add(layers.Flatten())
-model.add(layers.Dense(256))
+model.add(layers.Dense(1024))
 model.add(layers.Activation("relu"))
-model.add(layers.Dropout(0.5))
 model.add(layers.Dense(num_categories))
 model.add(layers.Activation("softmax"))
 
 model.compile(loss="categorical_crossentropy", optimizer="Adam", metrics=["accuracy"])
 
-history = model.fit(X_train, y_train, epochs=10, batch_size=32)
+history = model.fit(X_train, y_train, epochs=10, batch_size=128, validation_data=(X_test, y_test))
 
 plt.plot(history.history["accuracy"], label="accuracy")
 plt.xlabel("Epoch")
 plt.ylabel("Accuracy")
+plt.legend(loc='lower right')
+
+test_loss, test_acc = model.evaluate(X_test,  y_test, verbose=2)
+print(f"Test accuracy = {test_acc}")
+
 plt.show()
--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 to do:
 - update network to improve accuracy
 Epoch 10/10
-549/549 [==============================] - 359s 653ms/step - loss: 1.7109 - accuracy: 0.4222
+549/549 [==============================] - 359s 653ms/step - loss: 1.7109 - accuracy: 0.4222 -> Batch size of 32 and 2 conv. layers.
