e32093eb7eb3052365e4dfff92573bf134730420
DennisLent <62008839+DennisLent@users.noreply.github.com>
2023-03-15 22:08:33 +0100

    update model

--- ComputerVision.py
+++ ComputerVision.py
@@ -6,7 +6,7 @@ from keras import layers, models
 from keras.utils import to_categorical
 import matplotlib.pyplot as plt
 
-image_size = 75
+image_size = 64
 
 data, categories = create_data(image_size)
 #print(data[0], np.shape(data[0][0]))
@@ -23,33 +23,50 @@ for features, label in data:
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=23)
 X_train, X_test = np.array(X_train), np.array(X_test)
-y_train, y_test = to_categorical(y_train, num_classes=num_categories), to_categorical(y_test, num_classes=num_categories)
+y_train_cat, y_test_cat = to_categorical(y_train, num_classes=num_categories), to_categorical(y_test, num_classes=num_categories)
+
+#Short overview of test set
+def show_overview():
+    plt.figure(figsize=(10,10))
+    for i in range(25):
+        plt.subplot(5,5,i+1)
+        plt.xticks([])
+        plt.yticks([])
+        plt.grid(False)
+        plt.imshow(X_train[i])
+        plt.xlabel(categories[y_train[i]])
+    print(f"Shape of X_train: {np.shape(X_train)} \n shape of X_test: {np.shape(X_test)}")
+    print(f"Shape of y_train: {np.shape(y_train)} \n Shape of y_test: {np.shape(y_test)}")
+    plt.show()
+
+#show_overview()
 
 
 #build model
 model = models.Sequential(name="AnimalRecognition")
-model.add(layers.Conv2D(32, (3, 3), padding="same", activation='relu', input_shape=(image_size, image_size, 3)))
+model.add(layers.Conv2D(128, (3, 3), padding="same", activation='relu', input_shape=(image_size, image_size, 3)))
 model.add(layers.MaxPooling2D((2, 2)))
 model.add(layers.Conv2D(64, (3, 3), activation='relu'))
 model.add(layers.MaxPooling2D((2, 2)))
 model.add(layers.Conv2D(64, (3, 3), activation='relu'))
 
 model.add(layers.Flatten())
-model.add(layers.Dense(1024))
-model.add(layers.Activation("relu"))
 model.add(layers.Dense(num_categories))
 model.add(layers.Activation("softmax"))
 
+model.summary()
+
 model.compile(loss="categorical_crossentropy", optimizer="Adam", metrics=["accuracy"])
 
-history = model.fit(X_train, y_train, epochs=10, batch_size=128, validation_data=(X_test, y_test))
+history = model.fit(X_train, y_train_cat, epochs=15, validation_data=(X_test, y_test_cat))
 
 plt.plot(history.history["accuracy"], label="accuracy")
+plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
 plt.xlabel("Epoch")
 plt.ylabel("Accuracy")
 plt.legend(loc='lower right')
 
-test_loss, test_acc = model.evaluate(X_test,  y_test, verbose=2)
+test_loss, test_acc = model.evaluate(X_test,  y_test_cat, verbose=2)
 print(f"Test accuracy = {test_acc}")
 
 plt.show()
--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Deep Learning
 to do:
 - update network to improve accuracy
-Epoch 10/10
-549/549 [==============================] - 359s 653ms/step - loss: 1.7109 - accuracy: 0.4222 -> Batch size of 32 and 2 conv. layers.
+- v2 (->conv32, pooling2,2, conv32, pooling2,2, conv64, flatten, dense10) with Test accuracy = 0.5235002040863037 (10 epochs)
+- v3 (->conv32, pooling2,2, conv32, pooling2,2, conv64, flatten, dense64, dense10) with Test accuracy = 0.3882308006286621 (10 epochs)
+- v3 (->conv64, pooling2,2, conv64, pooling2,2, conv128, flatten, dense10) with Test accuracy = 0.5475735664367676 (10 epochs)
+- v4 (->conv128, pooling2,2, conv64, pooling2,2, conv64, flatten, dense10) with Test accuracy = 0.5735574960708618 (10 epochs)
