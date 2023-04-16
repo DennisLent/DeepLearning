c73aff22aca53ecae4dbec05ca10f74b067eb92f
DennisLent <dennis.lentschig955@gmail.com>
2023-04-17 00:04:47 +0200

    implemented feed forward and feed backward

--- Layers.py
+++ Layers.py
@@ -8,7 +8,16 @@ class Linear():
     
     def feed_forward(self, x):
         y = (x @ self.weight) + self.bias
+        self.cache = x
         return y
+    
+    def feed_backward(self, dup):
+        x = self.cache
+        dx = dup @ self.weight.transpose()
+        self.weight_gradient = x.transpose() @ dup
+        self.bias_gradient = np.sum(dup, axis=0)
+
+        return dx
     
     def __str__(self) -> str:
         return f"Linear Layer: w = {np.shape(self.weight)} b = {np.shape(self.bias)}"
@@ -17,7 +26,14 @@ class ReLU():
     
     def feed_forward(self, x):
         y = np.maximum(0, x)
+        self.cache = y
         return y
+    
+    def feed_backward(self, dup):
+        dx = dup.copy()
+        y = self.cache
+        dx[y == 0] = 0
+        return dx
 
     def __str__(self) -> str:
         return "ReLU"
@@ -28,8 +44,15 @@ class LeakyReLU():
         self.cutoff = cutoff
     
     def feed_forward(self, x):
-        y = np.maximum(self.cutoff, x)
+        y = np.maximum(self.cutoff*x, x)
+        self.cache = y
         return y
+    
+    def feed_backward(self, dup):
+        dx = dup.copy()
+        y = self.cache
+        dx[y<=0] = self.cutoff*dx[y<=0]
+        return dx
 
     def __str__(self) -> str:
         return f"Leaky ReLU cut = {self.cutoff}"
@@ -38,7 +61,13 @@ class Sigmoid():
 
     def feed_forward(self, x):
         y = 1/(1 + np.exp(-x))
+        self.cache = y
         return y
+    
+    def feed_backward(self, dup):
+        y = self.cache
+        dx = y*(1-y)*dup
+        return dx
 
     def __str__(self) -> str:
         return "Sigmoid"
@@ -47,17 +76,36 @@ class Tanh():
 
     def feed_forward(self, x):
         y = np.tanh(x)
+        self.cache = y
+        return y
+    
+    def feed_backward(self, dup):
+        y = self.cache
+        dx = (1-np.power(y, 2))*dup
+        return dx
     
     def __str__(self) -> str:
         return "Tanh"
 
 if __name__ == "__main__":
-    n_samples, in_features, out_features = 2, 3, 4
-    x = np.random.rand(n_samples, in_features)
+    
+    def test_backward():
+        n_samples, input_dim, output_dim = 2,3,4
+        layer = Linear(input_dim, output_dim)
+        x = np.random.rand(n_samples,input_dim)
+        dy = np.random.rand(n_samples, output_dim)
+        y = layer.feed_forward(x)
+        dx = layer.feed_backward(dy)
+        print(f"y = {y} of shape: {np.shape(y)}")
+        print(f"dx = {dx} of shape: {np.shape(dx)}")
+    
+    def test_attributes():
+        layers = [Linear(3,2), ReLU(), Linear(5,2), Sigmoid()]
+        for layer in layers:
+            if hasattr(layer, "weight"):
+                print("it has")
+    
+    
+    test_backward()
+    test_attributes()
 
-    layer = Linear(in_features, out_features)
-    print(np.shape(x))
-    print(layer)
-    y = layer.feed_forward(x)
-
-    print('Shape of ouput tensor y:', np.shape(y))
--- Network.py
+++ Network.py
@@ -10,13 +10,30 @@ class DeepNetwork():
         for layer in self.layers:
             x = layer.feed_forward(x)
         return x
+    
+    def feed_backward(self, dx):
+        for layer in reversed(self.layers):
+            dx = layer.feed_backward(dx)
+        return dx
+    
+    def optimizer(self, learn_rate):
+        for layer in self.layers:
+            if hasattr(layer, "weight"):
+                layer.weight = layer.weight - learn_rate*layer.weight_gradient
+            if hasattr(layer, "bias"):
+                layer.bias = layer.bias - learn_rate*layer.bias_gradient
 
 
 if __name__ == "__main__":
-    n_samples, input_dim, output_dim = 2,3,4
-    layer_list = [Layers.Linear(input_dim,5),Layers.ReLU(), Layers.Linear(5,output_dim)]
-    net = DeepNetwork(layer_list)
-    x = np.random.rand(n_samples,input_dim)
-    y = net.feed_forward(x)
-    print(f"y = {y}")
-    print(np.shape(y))
+
+    def test_forward():
+        n_samples, input_dim, output_dim = 2,3,4
+        layer_list = [Layers.Linear(input_dim,5),Layers.ReLU(), Layers.Linear(5,output_dim)]
+        net = DeepNetwork(layer_list)
+        x = np.random.rand(n_samples,input_dim)
+        y = net.feed_forward(x)
+        print(f"y = {y}")
+        print(np.shape(y))        
+        
+    
+    test_forward()
