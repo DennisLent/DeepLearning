33e51ab35332df3ed5ff2f230448facd7d76bbb7
DennisLent <dennis.lentschig955@gmail.com>
2023-06-30 20:21:29 +0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 import os
 import urllib.request
 import tarfile
+import gdown
+import zipfile
 
 url_cifar = "https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz"
 filename_cifar = "cifar-10-python.tar.gz"
+url_mnist = "https://drive.google.com/file/d/1jiFRYsI6UczzVn5uUj-zMA_PNlBhlkvX/view?usp=sharing"
+filename_mnist = "mnist"
 
 if not os.path.exists("cifar-10-batches-py"):
     print(f"downloading {filename_cifar}...")
@@ -14,3 +18,11 @@ with tarfile.open(filename_cifar, "r:gz") as tar:
     tar.extractall()
 
 os.remove(filename_cifar)
+
+if not os.path.exists("mnist"):
+    os.makedirs("mnist")
+    print(f"downloading {filename_mnist}...")
+    gdown.download(url_mnist, output="mnist.zip", quiet=False)
+
+    output = zipfile.ZipFile("mnist.zip")
+    output.extractall("mnist")
